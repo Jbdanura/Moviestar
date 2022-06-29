74e7263375f6d503270dc9e192d581f7502d9c1f
unknown <danurabe@gmail.com>
2022-06-29 13:51:09 -0300

    commit

---
+++ front/src/Components/slider.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Carousel } from 'react-bootstrap';
+
+const Slider = ({trending}) => {
+    return(
+        <Carousel>
+        {trending.slice(0,10).map((movie => {
+           const imageSrc = `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`
+           return <Carousel.Item>   
+                 <img
+                    className="d-block w-100 carousel-img"
+                    src={imageSrc}
+                    alt={movie.original_title}
+                 />
+              <Carousel.Caption>
+                 <h2>{movie.original_title}</h2>
+              </Carousel.Caption>
+           </Carousel.Item>
+        }))}
+     </Carousel>
+    )
+}
+
+export default Slider
---
+++ front/src/Components/trending.js
@@ -0,0 +1,24 @@
+import React from "react"
+import { Card, Button } from "react-bootstrap"
+
+const Trending = ({trending}) => {
+    return (
+        <div className="trending-container">
+            {trending.map(movie=>{
+                const imageSrc = `https://image.tmdb.org/t/p/original/${movie.backdrop_path}`
+                return <Card>
+                <Card.Img variant="top" src={imageSrc} />
+                <Card.Body>
+                  <Card.Title>{movie.original_title}</Card.Title>
+                  <Card.Text>
+                    {movie.overview}
+                  </Card.Text>
+                  <Button variant="primary">View information</Button>
+                </Card.Body>
+              </Card>
+            })}
+        </div>
+    )
+}
+
+export default Trending
